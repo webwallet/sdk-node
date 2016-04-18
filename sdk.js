04aab22471f785228a4542ccae45d1d93fd3004c
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-04-18 16:37:14 -0500

    Added comments and extended package.json

--- sdk.js
+++ sdk.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var crypto    = require('crypto');
-var elliptic  = require('elliptic');
-var ripemd160 = require('ripemd160');
-var request   = require('request');
-var bs58check = require('bs58check');
-var P         = require('bluebird');
+const P         = require('bluebird');
+const bs58check = require('bs58check');
+const crypto    = require('crypto');
+const elliptic  = require('elliptic');
+const request   = require('request');
+const ripemd160 = require('ripemd160');
+const urlModule = require('url');
 
 module.exports = {
   generate: {
@@ -27,20 +28,30 @@ module.exports = {
 
 /*--------------------------------------------------------------------------------*/
 
+/** Generates and new key pair, derives an address and a signs a registration statement
+ * @param  {Object} options - Cryptographic scheme and address type
+ * @return {Object} wallet - Wallet address, public/private keys and a signed statement
+ */
 function generateWalletAddress(options) {
   options = options || {};
 
-  var keys = generateKeyPair(options.scheme || 'ed25519');
-  var address = deriveWalletAddress(keys.public, options.type);
-  var statement = createAddressRegistrationStatement(address, keys, options);
+  let keys = generateKeyPair(options.scheme || 'ed25519');
+  let address = deriveWalletAddress(keys.public, options.type);
+  let statement = createAddressRegistrationStatement(address, keys, options);
 
-  return {
+  let wallet = {
     address: address,
     keys: keys,
     statement: statement
-  }
+  };
+
+  return wallet;
 }
 
+/** Generates a new key pair, derives a currency issuer address and signs a registration statement
+ * @param  {Object} options - Cryptographic scheme and address type
+ * @return {Object} - Currency issuer address, public/private keys and a signed statement
+ */
 function generateCurrencyIssuer(options) {
   options = options || {};
   options.type = 'issuer';
@@ -48,21 +59,33 @@ function generateCurrencyIssuer(options) {
   return generateWalletAddress(options);
 }
 
+/** Generates a transaction request document in the form of an IOU
+ * @param {Object} signer - Wallet whose private key is to be used for signing the IOU
+ * @param {Object} transaction - Transaction parameters
+ * @param {Object} options
+ * @return {Object} - A cryptographically signed IOU
+ */
 function generateTransactionRequest(signer, transaction, options) {
-  var iou = {
+  let iou = {
     amt: transaction.amount,
     cur: transaction.currency,
     sub: signer.address,
     aud: transaction.destination,
-    nce: Math.floor(Math.random() * 1000000000) + ''
+    nce: String(Math.floor(Math.random() * 1000000000))
   };
 
   return createTransactionRequestStatement(signer, iou, options);
 }
 
+/** Sends a wallet address registration request to the supplied URL
+ * @param {string} url - The URL of a webwallet server 
+ * @param {Object} body - A wallet address registration statement
+ * @param {Object} options - Request parameters such as method and headers
+ * @return {Promise} - Resolves to the response body
+ */
 function registerWalletAddress(url, body, options) {
   options = options || {};
-  url += '/address';
+  url = resolveURL(url, '/address');
 
   return new P(function (resolve, reject) {
     request({
@@ -78,9 +101,15 @@ function registerWalletAddress(url, body, options) {
   });
 }
 
+/** Sends a currency issuer registration request to the supplied URL
+ * @param {string} url - The URL of a webwallet server
+ * @param {Object} body - A currency issuer registration statement
+ * @param {Object} options - Request parameters such as method and headers
+ * @return {Promise} - Resolves to the response body
+ */
 function registerCurrencyIssuer(url, body, options) {
   options = options || {};
-  url += '/currency';
+  url = resolveURL(url, '/currency');
 
   return new P(function (resolve, reject) {
     request({
@@ -96,9 +125,15 @@ function registerCurrencyIssuer(url, body, options) {
   });
 }
 
+/** Sends a transaction request to the supplied URL
+ * @param {string} url - The URL of a webwallet server
+ * @param {Object} body - A transaction request statement
+ * @param {Object} options - Request parameters such as method and headers
+ * @return {Promise} - Resolves to the response body
+ */
 function registerTransactionRequest(url, body, options) {
   options = options || {};
-  url += '/transaction';
+  url = resolveURL(url, '/transaction');
 
   return new P(function (resolve, reject) {
     request({
@@ -114,10 +149,16 @@ function registerTransactionRequest(url, body, options) {
   });
 }
 
+/** Checks the balance of a wallet address
+ * @param {string} url - The URL of a webwallet server
+ * @param {string} address - A wallet address
+ * @param {Object} options - Request parameters such as headers
+ * @return {Promise} - Resolves to the response body
+ */
 function checkAddressBalance(url, address, options) {
   options = options || {};
-  url += '/address/.../balance'.replace('...', address);
-  console.log(url)
+  let path = '/address/.../balance'.replace('...', address);
+  url = resolveURL(url, path);
 
   return new P(function (resolve, reject) {
     request({
@@ -133,15 +174,19 @@ function checkAddressBalance(url, address, options) {
 
 /*--------------------------------------------------------------------------------*/
 
-var hashes = ['sha256', 'sha512'];
-var schemes = {
+const hashes = ['sha256', 'sha512'];
+const schemes = {
   'ed25519': new elliptic.ec('ed25519'),
   'secp256k1': new elliptic.ec('secp256k1')
 };
 
+/** Generates a pair of cryptographic keys
+ * @param {string} scheme - A cryptographic scheme
+ * @return {Object} keys - A cryptographic key pair
+ */
 function generateKeyPair(scheme) {
-  var keypair;
-  var keys;
+  let keypair;
+  let keys;
 
   switch (scheme) {
     case 'ed25519':
@@ -154,27 +199,39 @@ function generateKeyPair(scheme) {
       };
       break;
     default:
-      return P.reject('invalid-scheme');
+      throw new Error('invalid-scheme');
       break;
   }
 
   return keys;
 }
 
+/** Derives a wallet address from a public key
+ * @param {<type>} publicKey - A public key to derive the address from
+ * @param {string} type - The type of wallet address to derive
+ * @return {string} base58public - A base58check encoded wallet address
+ */
 function deriveWalletAddress(publicKey, type) {
-  var keyBuffer = new Buffer(publicKey, 'hex');
-  var firstHash = crypto.createHash('sha256').update(keyBuffer).digest();
-  var secondHash = ripemd160(firstHash);
-  var extendedHash = (type === 'issuer' ? '57' : '87') + secondHash.toString('hex');
-  var base58Public = bs58check.encode(new Buffer(extendedHash, 'hex'));
+  let keyBuffer = new Buffer(publicKey, 'hex');
+  let firstHash = crypto.createHash('sha256').update(keyBuffer).digest();
+  let secondHash = ripemd160(firstHash);
+  let extendedHash = (type === 'issuer' ? '57' : '87') + secondHash.toString('hex');
+  let base58Public = bs58check.encode(new Buffer(extendedHash, 'hex'));
 
   return base58Public;
 }
 
+/** Creates and signs a statement to be sent for registering an address
+ * @param {string} address - A wallet address
+ * @param {Object} keys - An object containing a cryptograhic key pair
+ * @param {Object} options - Parameters such as hash type
+ * @return {Object} - A cryptographically signed statement
+ */
 function createAddressRegistrationStatement(address, keys, options) {
   options = options || {};
 
-  var jws = {
+  /* Create an extended JSON Web Signatures object */
+  let jws = {
     hash: {
       type: (hashes.indexOf(options.hash) > -1) ? options.hash : 'sha256',
       value: ''
@@ -197,19 +254,28 @@ function createAddressRegistrationStatement(address, keys, options) {
     ]
   };
 
+  /* Generate a cryptogrphic hash of the payload */
   jws.hash.value = crypto.createHash(jws.hash.type)
     .update(JSON.stringify(jws.payload)).digest('hex');
 
+  /* Sign the hash of the payload */
   jws.signatures[0].signature = schemes[keys.scheme]
     .sign(jws.hash.value, keys.private, 'hex').toDER('hex');
 
   return jws;
 }
 
+/** Creates a transaction request statement in the form of an IOU
+ * @param {Object} signer - Wallet whose private key is to be used for signing the IOU
+ * @param {Object} iou - The IOU to sign
+ * @param {Object} options - Parameters such as hash type
+ * @return {Object} - A cryptographically signed IOU
+ */
 function createTransactionRequestStatement(signer, iou, options) {
   options = options || {};
 
-  var jws = {
+  /* Create an extended JSON Web Signatures object */
+  let jws = {
     hash: {
       type: (hashes.indexOf(options.hash) > -1) ? options.hash : 'sha256',
       value: ''
@@ -226,11 +292,36 @@ function createTransactionRequestStatement(signer, iou, options) {
     ]
   };
 
+  /* Generate a cryptogrphic hash of the payload */
   jws.hash.value = crypto.createHash(jws.hash.type)
     .update(JSON.stringify(jws.payload)).digest('hex');
 
+  /* Sign the hash of the payload */
   jws.signatures[0].signature = schemes[signer.keys.scheme]
     .sign(jws.hash.value, signer.keys.private, 'hex').toDER('hex');
 
   return jws;
+}
+
+const protocols = ['http', 'https', 'http:', 'https:'];
+/** Resolves a URL given a path
+ * @param {string} url - URL to resolve
+ * @param {string} path - Path to append to base URL
+ * @return {string} resolvedUrl - A valid URL
+ */
+function resolveURL(url, path) {
+  if (typeof url !== 'string' || typeof path !== 'string') {
+    return new Error('url-and-path-required');
+  }
+
+  /* Remove leading and duplicate slashes */
+  url = url.replace(/\/{2,}/g, '/').replace(/^\//, '').replace(':/','://');
+
+  let parsedUrl = urlModule.parse(url);
+  if (protocols.indexOf(parsedUrl.protocol) < 0 || !parsedUrl.host) {
+    return new Error('invalid-url');
+  }
+
+  let resolvedUrl = urlModule.resolve(parsedUrl.href, path);
+  return resolvedUrl;
 }
